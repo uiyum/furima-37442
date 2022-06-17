bfc582941601955f6d24bf85d305bd026d5bc504
uiyum <pink.y7u4m@gmail.com>
2022-06-17 13:10:51 +0900

    バリデーション修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,7 +10,8 @@ class User < ApplicationRecord
   validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください' 
 
   with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: '全角文字を使用してください' } do
-    validates :first_name, :last_name,
+    validates :first_name, :last_name
+  end
 
   with_options presence: true, format: { with: /\A[ァ-ヶ]+\z/, message: '全角カタカナを使用してください' } do
     validates :first_name_kana, :last_name_kana
