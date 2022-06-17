161a223ff5db5e5faa49642d0a0d77766548b40a
uiyum <pink.y7u4m@gmail.com>
2022-06-17 16:35:35 +0900

    first commit

--- app/controllers/sells_controller.rb
+++ app/controllers/sells_controller.rb
@@ -1,3 +1,11 @@
 class SellsController < ApplicationController
     before_action :authenticate_user!, except: [:index]
 end
+
+def index
+    @sells = Sell.all
+end
+
+def new
+    @sell = Sell.new
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'sells#index'
+  resources :sells, only: [:index, :new]
 end
