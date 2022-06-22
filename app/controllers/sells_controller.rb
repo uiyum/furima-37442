6f7146575075d820ee56698c0ce68189e87ca49f
uiyum <pink.y7u4m@gmail.com>
2022-06-22 22:58:30 +0900

    java実装＆出品成功

--- app/controllers/sells_controller.rb
+++ app/controllers/sells_controller.rb
@@ -1,27 +1,26 @@
 class SellsController < ApplicationController
     before_action :authenticate_user!, except: [:index]
-end
 
-def index
-    @sell = Sell.all
-    #（応用）メッセージ機能を実装しよう、問題４使う？
-end
-
-def new
-    @sell = Sell.new
-end
-
-def create
-    @user = User.find(params[:user_id])
-    @sell = @user.sells,new(sell_params)
-    if @sell.save
-        redirect_to root_path(@sell)#path後で見る
-    else
-        render :index
+    def index
+        @sell = Sell.all
     end
-end
 
-private
-def sell_params
-    params.require(:sell).permit(:good_name, :image, :text, :category_id, :condition_id,:delivery_id, :postage_id, :prefecture_id).marge(user_id: current_user.id)
+    def new
+        @sell = Sell.new
+    end
+
+    def create
+        @sell = Sell.new(sell_params)
+        if  @sell.save
+            redirect_to root_path
+        else
+            render new_sell_path
+        end
+    end
+
+    private
+    def sell_params
+        params.require(:sell).permit(:good_name, :image, :text, :category_id, :condition_id,:delivery_id, :postage_id, :prefecture_id, :price).merge(user_id: current_user.id)
+    end
+   
 end
