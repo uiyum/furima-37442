57a00ba0bd21cca76a39b2192e654a84e4d14e52
uiyum <pink.y7u4m@gmail.com>
2022-06-23 15:26:17 +0900

    テストコード実装

--- app/models/sell.rb
+++ app/models/sell.rb
@@ -8,4 +8,9 @@ class Sell < ApplicationRecord
     belongs_to :category, :condition, :delivery, :postage, :prefecture
     validates :category_id, :condition_id, :delivery_id, :postage_id, :prefecture_id, numericality: { other_than: 1 , message: "can't be blank"}
 
+    with_options presence: true, format: { with: /\A[0-9]+\z/ } do
+        validates :price, numericality: {only_integer: true, greater_than_or_equal_to:300,less_than_or_equal_to:9_999_999},
+        presence: {message: "can't be blank"}
+    end
+
 end
